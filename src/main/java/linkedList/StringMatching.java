7831751f566fc66a35fa6dd300e008c07f9434fc
AizadaNY <aizada.abdyrakhmanova@gmail.com>
2024-06-04 23:02:44 -0400

    LeetCode

--- src/main/java/linkedList/StringMatching.java
+++ src/main/java/linkedList/StringMatching.java
@@ -1,2 +1,36 @@
-package linkedList;public class StringMatching {
+package linkedList;
+
+import java.util.Scanner;
+
+public class StringMatching {
+
+    /*
+    Given a string and a pattern, your task is to count the number of positions where the pattern occurs in the string.
+    Input
+    The first input line has a string of length n, and the second input line has a pattern of length m. Both of them consist of characters a–z.
+    Output
+    Print one integer: the number of occurrences.
+    Constraints
+
+    1 \le n,m \le 10^6
+
+    Example
+    Input:
+    saippuakauppias
+    pp
+
+    Output:
+    2
+     */
+
+    /*
+    1.Constant
+    2.target word convert to hash code
+    3.string convert to hashcode
+    4.
+     */
+    Scanner scanner=new Scanner(System.in);
+    String s=scanner.next();
+    String t=scanner.next();
+    Long h_t;
 }
