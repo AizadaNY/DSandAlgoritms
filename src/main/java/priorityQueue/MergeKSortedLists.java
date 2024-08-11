649cb629f913b686a4e0eb33088a73b8eb327713
AizadaNY <aizada.abdyrakhmanova@gmail.com>
2024-08-10 21:41:44 -0400

    LeetCode Linked List Cycle

--- src/main/java/priorityQueue/MergeKSortedLists.java
+++ src/main/java/priorityQueue/MergeKSortedLists.java
@@ -53,33 +53,33 @@ public class MergeKSortedLists {
      * }
      */
 
-    public <ListNode> ListNode mergeKLists(ListNode[] lists) {
-
-        if(lists==null|| lists.length==0){
-            return null;
-        }
-
-        PriorityQueue<ListNode> q=new PriorityQueue<ListNode>(lists.length,(a, b)-> a.val-b.val);
-
-        ListNode dummy=new ListNode(0);
-        ListNode tail=dummy;
-
-        for(ListNode node:lists){
-            if(node!=null){
-                q.add(node);
-            }
-        }
-
-        while(!q.isEmpty()){
-            tail.next=q.poll();
-            tail=tail.next;
-
-            if(tail.next!=null){
-                q.add(tail.next);
-            }
-        }
-        return dummy.next;
-
-
-    }
+//    public <ListNode> ListNode mergeKLists(ListNode[] lists) {
+//
+//        if(lists==null|| lists.length==0){
+//            return null;
+//        }
+//
+//        PriorityQueue<ListNode> q=new PriorityQueue<ListNode>(lists.length,(a, b)-> a.val-b.val);
+//
+//        ListNode dummy=new ListNode(0);
+//        ListNode tail=dummy;
+//
+//        for(ListNode node:lists){
+//            if(node!=null){
+//                q.add(node);
+//            }
+//        }
+//
+//        while(!q.isEmpty()){
+//            tail.next=q.poll();
+//            tail=tail.next;
+//
+//            if(tail.next!=null){
+//                q.add(tail.next);
+//            }
+//        }
+//        return dummy.next;
+//
+//
+//    }
 }
