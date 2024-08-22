9d6ab28a0407634f79b6b84f93319a498264a557
AizadaNY <aizada.abdyrakhmanova@gmail.com>
2024-08-21 22:10:03 -0400

    string compression

---
+++ src/main/java/StringCompression.java
@@ -0,0 +1,35 @@
+public class StringCompression {
+
+    public int compress(char[] chars) {
+        int n = chars.length;
+        int start=0;
+        int index=0;
+
+        while(start<n){
+            char streakChar=chars[start];
+
+            int end=start;
+            int count=1;
+            while(end + 1 < n && chars[end]==streakChar ){
+                end++;
+                count++;
+            }
+
+            chars[index]=streakChar;
+            index++;
+
+            if(count>1){
+                char[] countAsChars=Integer.toString(count).toCharArray();
+                for(char digit:countAsChars){
+                    chars[index] = digit;
+                    index++;
+                }
+            }
+
+            start=end+1;
+
+        }
+        return index;
+    }
+
+}
